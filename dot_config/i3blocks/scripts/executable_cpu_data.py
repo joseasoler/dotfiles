6918be21f307ab65c70950967ee59a7b52c14aa1
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2020-11-21 18:33:17 +0100

    Refactor cpu_data.py to accept a sensor argument. Temporarily use k10temp for the core temperature until the Linux kernel 5.10 is released.

--- dot_config/i3blocks/scripts/executable_cpu_data.py
+++ dot_config/i3blocks/scripts/executable_cpu_data.py
@@ -3,6 +3,7 @@
 import mouse
 from os import path
 from processor import ProcessorData, processor_status
+import sys
 from subprocess import run
 
 core = 'coretemp'
@@ -12,12 +13,13 @@ util_data = ProcessorData(args=('python', '-c', 'import psutil; print(psutil.cpu
                                       (' <span color="yellow">{}%</span>', 60.0),
                                       (' <span color="white">{}%</span>',)))
 
-temp_data = ProcessorData(args=('{}/cpu_temperature.py'.format(path.dirname(__file__)), core,),
-                          thresholds=((' <span color="red">{}°C</span>', 90.0),
-                                      (' <span color="yellow">{}°C</span>', 70.0),
-                                      (' <span color="white">{}°C</span>',)))
-
 if __name__ == "__main__":
+    core = '' if len(sys.argv) == 0 else sys.argv[1]
+    temp_data = ProcessorData(args=('{}/cpu_temperature.py'.format(path.dirname(__file__)), core,),
+                              thresholds=((' <span color="red">{}°C</span>', 90.0),
+                                          (' <span color="yellow">{}°C</span>', 70.0),
+                                          (' <span color="white">{}°C</span>',)))
+
     if mouse.button() == mouse.BUTTON_LEFT:
         run(['alacritty', '-e', 'bpytop', '-s'])
     print(processor_status(util_data, temp_data))
--- dot_config/i3blocks/scripts/executable_cpu_temperature.py
+++ dot_config/i3blocks/scripts/executable_cpu_temperature.py
@@ -4,11 +4,9 @@ from psutil import sensors_temperatures
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) <= 1:
+    temperature_map = sensors_temperatures()
+    if len(sys.argv) <= 1 or sys.argv[1] not in temperature_map:
         print('300')
     else:
-        temperature_list = sensors_temperatures()[sys.argv[1]]
-        for temperature in temperature_list:
-            if 'Package' in temperature.label:
-                print(temperature.current)
-                break
+        # ToDo Improve with the data of the Zen 3 temperature sensor when the 5.10 kernel is released.
+        print(temperature_map[sys.argv[1]][0].current)
